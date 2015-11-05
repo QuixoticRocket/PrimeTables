11e0ab0de12ca331da2655cd6140727406970c18
Alex <quixoticrocket@gmail.com>
2015-11-05 13:58:39 +0000

    Test 2 green

--- PrimeNumberLogic/PrimeNumberGenerator.cs
+++ PrimeNumberLogic/PrimeNumberGenerator.cs
@@ -9,11 +9,32 @@ namespace PrimeNumberLogic
 {
     internal class PrimeNumberGenerator : INumberGenerator
     {
+        private List<int> foundPrimes = new List<int>();
+
         //generates prime numbers
         public int[] GetNumbers(uint numberToGet)
         {
             int[] answers = new int[numberToGet];
 
+            int i = 0;
+            while(foundPrimes.Count < numberToGet)
+            {
+                int foundprime = 0;
+                if(i == 0)
+                {
+                    foundprime = 2;
+                }
+                else
+                {
+                    //more complicated code for finding primes here (we can't just map case statement to a list of prime ;)
+                    throw new NotImplementedException();
+                }
+
+                foundPrimes.Add(foundprime);
+                i++;
+            }
+
+            answers = foundPrimes.ToArray();
             return answers;
         }
     }
