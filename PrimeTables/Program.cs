745d49c866099cb6d653319fbf5f9e7cad44916a
Alex <quixoticrocket@gmail.com>
2015-11-05 16:05:11 +0000

    Implementing the simple display layer (console)

--- PrimeTables/Program.cs
+++ PrimeTables/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PrimeNumberLogic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,80 @@ namespace PrimeTables
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please supply n (n must be greater than 0):");
+            string input = Console.ReadLine();
+            int n = 0;
+
+            int.TryParse(input, out n);
+
+            while(n < 1)
+            {
+                Console.WriteLine("Sorry. Couldn't parse that number or it was less than 1");
+                Console.WriteLine("Please supply n (n must be greater than 0):");
+                input = Console.ReadLine();
+                int.TryParse(input, out n);
+            }
+
+            PrimeNumberEngine engine = new PrimeNumberEngine();
+            int?[,] output = engine.GenerateTable((uint)n);
+
+            //output
+
+            Console.WriteLine();
+
+            int maxstringlength = 0;
+
+            //just to make display nice we need to find maximum string length
+            for (int x = 0; x < n + 1; x++)
+            {
+                for (int y = 0; y < n + 1; y++)
+                {
+                    if(output[x,y].HasValue)
+                    {
+                        string tempstring = output[x, y].ToString();
+                        maxstringlength = tempstring.Length > maxstringlength ? tempstring.Length : maxstringlength;
+                    }
+                }
+            }
+
+                    for (int x = 0; x < n+1; x++)
+            {
+                for (int y = 0; y < n+1; y++)
+                {
+                    Console.Write(PaddedString(output[x, y], maxstringlength));
+                    Console.Write("|");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("please press any key to exit");
+            Console.ReadKey();
+        }
+
+        private static string PaddedString(int? v, int maxstringlength)
+        {
+            int pad = 2 + (maxstringlength % 2); //variable padding to keep the string an even number
+            char[] charArray = new char[maxstringlength + pad];
+            int midpoint = (maxstringlength + 2) / 2;
+            string numberAsText = v.HasValue ? v.Value.ToString() : string.Empty;
+            int textlength = numberAsText.Length;
+
+            int prepadlength = (maxstringlength + pad - textlength) / 2;
+
+            for (int i = 0; i < maxstringlength + pad; i++)
+            {
+                if(i < prepadlength || i >= prepadlength + textlength)
+                {
+                    charArray[i] = ' ';
+                }
+                else
+                {
+                    charArray[i] = numberAsText[i - prepadlength];
+                } 
+            }
+
+            return new string(charArray);
         }
     }
 }
