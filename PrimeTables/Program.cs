d7a32a11581d053ab94e4e55599540bd07d2b923
Alex <quixoticrocket@gmail.com>
2015-11-05 16:11:46 +0000

    Making output display faster and use a little less memory (stringbuilder and reused char declerations)

--- PrimeTables/Program.cs
+++ PrimeTables/Program.cs
@@ -17,7 +17,7 @@ namespace PrimeTables
 
             int.TryParse(input, out n);
 
-            while(n < 1)
+            while (n < 1)
             {
                 Console.WriteLine("Sorry. Couldn't parse that number or it was less than 1");
                 Console.WriteLine("Please supply n (n must be greater than 0):");
@@ -39,7 +39,7 @@ namespace PrimeTables
             {
                 for (int y = 0; y < n + 1; y++)
                 {
-                    if(output[x,y].HasValue)
+                    if (output[x, y].HasValue)
                     {
                         string tempstring = output[x, y].ToString();
                         maxstringlength = tempstring.Length > maxstringlength ? tempstring.Length : maxstringlength;
@@ -47,22 +47,28 @@ namespace PrimeTables
                 }
             }
 
-                    for (int x = 0; x < n+1; x++)
+            StringBuilder sb = new StringBuilder();
+            char seperator = '|';
+            char space = ' ';
+
+            for (int x = 0; x < n + 1; x++)
             {
-                for (int y = 0; y < n+1; y++)
+                for (int y = 0; y < n + 1; y++)
                 {
-                    Console.Write(PaddedString(output[x, y], maxstringlength));
-                    Console.Write("|");
+                    sb.Append(PaddedString(output[x, y], maxstringlength, space));
+                    sb.Append(seperator);
                 }
-                Console.WriteLine();
+                sb.Append(Environment.NewLine);
             }
+
+            Console.Write(sb.ToString());
 
             Console.WriteLine();
             Console.WriteLine("please press any key to exit");
             Console.ReadKey();
         }
 
-        private static string PaddedString(int? v, int maxstringlength)
+        private static string PaddedString(int? v, int maxstringlength, char spacerChar)
         {
             int pad = 2 + (maxstringlength % 2); //variable padding to keep the string an even number
             char[] charArray = new char[maxstringlength + pad];
@@ -74,14 +80,14 @@ namespace PrimeTables
 
             for (int i = 0; i < maxstringlength + pad; i++)
             {
-                if(i < prepadlength || i >= prepadlength + textlength)
+                if (i < prepadlength || i >= prepadlength + textlength)
                 {
-                    charArray[i] = ' ';
+                    charArray[i] = spacerChar;
                 }
                 else
                 {
                     charArray[i] = numberAsText[i - prepadlength];
-                } 
+                }
             }
 
             return new string(charArray);
